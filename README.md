7bbd3789e8cab2df291afae87659c80664bcbce5
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2023-01-11 19:34:06 +0200

    full project first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# أذكار الصباح والمساء
+
+Islamic and Arabic App built to practice Vannilla JavaScript, it helps Muslims to do one there daily routine to read a certain Text with a specified repetetion.
+
+## Overview
+
+### The challenge
+
+- the app gets the Time automaticly based on users browser
+- if the time is before 03:00 pm the app will show morning Text and if it's after that it will show evening
+- users can use the button to count up till there each the specified amount for each text
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- [Live Site URL](https://husamasaad.github.io/currency-converter/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Vannila JavaScript
+- XML Http Request and JSON file
+
+## Author
+
+- github Profile - [Husam Asaad](https://github.com/husamasaad)
---
+++ app.js
@@ -0,0 +1,137 @@
+const title = document.getElementById("title");
+const nowTime = document.getElementById('time');
+const timeType = document.getElementById('timeType');
+const textDiv = document.querySelector('.text');
+const repBtn = document.getElementById('rpt-btn');
+const resetBtn = document.getElementById('reset');
+const repTimesContainer = document.querySelector('.rpt-times');
+const btns = document.querySelector('.btns');
+
+let repeat = 0;
+let nextIndex = 0;
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const now = new Date();
+  let hrs;
+  let hours = now.getHours();
+  let mins = (now.getMinutes() > 9) ? now.getMinutes() : `0${now.getMinutes()}` ;
+
+  if (hours > 12) {
+    hrs = ((hours - 12) > 9 )? hours - 12 : `0${(hours - 12)}`;
+  } else {
+    hrs = (hours > 9 )? hours : `0${hours}`;
+  }
+
+  if (hours > 15) {
+    getText("pm");
+    nowTime.textContent = `${hrs}:${mins} م`;
+  }
+  else {
+    getText("am");
+    nowTime.textContent = `${hrs}:${mins} ص`;
+    timeType.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+    title.textContent = "أذكار الصباح"
+  }
+
+  resetBtn.addEventListener("click", () => {
+    repeat = 0;
+    repBtn.textContent = 0;
+  })
+})
+
+
+
+
+function getText(tm) {
+  let xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      let obj =JSON.parse(this.responseText);
+
+      createText(obj[`${tm}`]);
+
+      repBtn.addEventListener('click', () => {
+        repeat++;
+        repBtn.textContent = repeat;
+        checkIfDone(obj[`${tm}`]);
+      })
+    }
+  }
+
+  xhr.open('GET', './data.json', true);
+  xhr.send();
+}
+
+function createText(obj) {
+  obj.forEach((element, index) => {
+    let div = document.createElement('div');
+    if (index === 0) {
+      div.classList.add('active');
+      updateTimesNumbers(element.times);
+    }
+    div.innerHTML = `${element.text} <span>${element.src}</span>`;
+    textDiv.appendChild(div);
+  });
+}
+
+function updateTimesNumbers(times) {
+  if (times === 1) {
+    repTimesContainer.innerHTML = '<span data-times="1" id="r-times"></span>مرة واحدة';
+  }
+  else if (times === 2) {
+    repTimesContainer.innerHTML = '<span data-times="2" id="r-times"></span>مرتان';
+  }
+  else if (times <= 10) {
+    repTimesContainer.innerHTML = `<span id="r-times">${times}</span> مرات`;
+  } else {
+    repTimesContainer.innerHTML = `<span id="r-times">${times}</span> مرة`;
+  }
+}
+
+function checkIfDone(obj) {
+  let times = document.getElementById('r-times');
+  let max;
+
+  if (times.textContent === "") {
+    max = times.dataset.times;
+  } else {
+    max = times.textContent;
+  }
+  
+  if (repeat == max) {
+    nextText(obj);
+  }
+  
+}
+
+function nextText(obj) {
+  let list = [...textDiv.children];
+
+  if ((nextIndex + 1) < list.length) {
+    list.forEach((item, index) => {
+      if (item.classList.contains('active')) {
+          item.classList.add('done');
+          item.classList.remove('active');
+          nextIndex = index + 1;
+          reset();
+          updateTimesNumbers(obj[nextIndex].times)
+        }
+    })
+    list[nextIndex].classList.add('active');
+  }
+  else {
+    endTheApp();
+  }
+}
+
+function reset() {
+  repeat = 0;
+  repBtn.textContent = 0;
+}
+
+function endTheApp() {
+  textDiv.innerHTML = "أحسنت, لقد أكملت أذكارك"
+  btns.innerHTML = "حفظك الله وبارك فيك";
+}
